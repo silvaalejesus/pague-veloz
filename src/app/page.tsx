44c32b895b7a208c4f333eec9c0000b080b47b91
silvaalejesus <silvaalejesus@gmail.com>
2025-07-16 04:10:15 -0300

    feat: listagem das corretoras na home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,39 @@
 import { ItemsList } from "@/components/ItemList";
+import { getAllCorretoras } from "@/services/corretoraService";
+import { Typography } from "@mui/material";
 
-export default function Home() {
+export default async function Home() {
+  const corretorasIniciais = await getAllCorretoras();
+
+  if (!corretorasIniciais || corretorasIniciais.length === 0) {
+    return (
+      <main>
+        <Typography
+          variant="h4"
+          component="h1"
+          gutterBottom
+          align="center"
+          sx={{ mt: 2 }}
+        >
+          Nenhuma corretora encontrada no momento.
+        </Typography>
+      </main>
+    );
+  }
+
   return (
-    <div>
-      <ItemsList />
-    </div>
+    <main>
+      <Typography
+        variant="h4"
+        component="h1"
+        gutterBottom
+        align="center"
+        sx={{ mt: 2 }}
+      >
+        Lista de Corretoras
+      </Typography>
+
+      <ItemsList initialItems={corretorasIniciais} />
+    </main>
   );
 }
